b07efcfaa34006ff40c7a9dd0f56020c939a4363
Fernando Velazco <daniel.fernando.velazco@gmail.com>
2024-10-11 22:25:43 -0500

    Se realiza formato de salida y peticiones

---
+++ src/Prinncipal.java
@@ -0,0 +1,9 @@
+public class Prinncipal {
+    public static void main(String[] args) {
+
+        ConsultaPelicula consulta = new ConsultaPelicula();
+        Pelicula pelicula = consulta.buscaPelicula(9);
+        System.out.println(pelicula);
+
+    }
+}
